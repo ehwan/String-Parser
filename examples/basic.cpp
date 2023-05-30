2d3601bc8268792efa33ebae565ec7e971c7c368
Taehwan Kim <ehwank98@gmail.com>
2023-05-30 14:38:46 +0900

    examples

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -3,17 +3,113 @@
 #include <string>
 #include <iostream>
 
-int main()
+namespace ep {
+  using namespace eh::parser;
+};
+
+// parsing variable-name pattern
+// [a-zA-Z_][a-zA-Z0-9_]*
+void variable_name_parse()
 {
-#if 0
+  auto small_alpha_parser = ep::range( 'a', 'z' );
+  auto big_alpha_parser = ep::range( 'A', 'Z' );
+  auto digit_parser = ep::range( '0', '9' );
+  auto underscore_parser = ep::one( '_' );
+
+  auto variable_parser =
+    ep::seq(
+        ep::or_(small_alpha_parser, big_alpha_parser, underscore_parser),
+        ep::repeat(
+          ep::or_(small_alpha_parser, big_alpha_parser, digit_parser, underscore_parser),
+          0,
+          99999
+        )
+    );
+
+  // this is same as variable_parser
+  auto variable_parser2 =
+    ( small_alpha_parser | big_alpha_parser | underscore_parser )
+    >>
+    *( small_alpha_parser | big_alpha_parser | digit_parser | underscore_parser )
+    ;
+
+  std::string str = "foo123 var456";
+  auto begin = str.begin();
+
+  // return optional<AttributeType>
+  auto ret = variable_parser.parse( begin, str.end() );
+  // begin points where parsing ends
+  std::string parsed_name( str.begin(), begin );
+
+  ++begin;
+  auto mid = begin;
+  auto ret2 = variable_parser2.parse( begin, str.end() );
+  std::string parsed_name2( mid, begin );
+
+  std::cout << parsed_name << std::endl;
+  std::cout << parsed_name2 << std::endl;
+}
+void dictionary()
+{
+  eh::parser::dictionary<int,char> dict;
+  dict.add( std::string("hello") , 0 );
+  dict.add( std::string("hell") , 2 );
+  dict.add( std::string("world") , 1 );
+  dict.add( std::string("hellowo") , 4 );
+
+  std::string str = "helloworld";
+  auto b = str.begin();
+  auto ret = dict.parse( b , str.end() );
+  if( ret )
+  {
+    std::cout << ret.get();
+  }else
+  {
+    std::cout << "fail";
+  }
+}
+void tokenize()
+{
+  // tokeninze string
+  std::string str = "   11 1aa    a2 22b     bb    ";
+
+  // white spaces parser; will be ignored when parsing
+  auto white = ' '_p;
+
+  // return 0 for alphabet, 1 for digit
+  auto parser = 
+    eh::parser::alpha[( [](int x){return 0;})] |
+    eh::parser::digit[( [](int x){return 1;})];
+
+  auto tokens = eh::parser::tokenize( parser , white , str.begin() , str.end() );
+  for( auto i : tokens )
+  {
+    if( i.data == 0 )
+    {
+      std::cout << "alpha " << *i.begin << std::endl;
+    }else if( i.data == 1 )
+    {
+      std::cout << "digit " << *i.begin << std::endl;
+    }else
+    {
+      std::cout << "unknown\n";
+    }
+  }
+}
+
+void comment_parser()
+{
   // commenting out
   auto printstring = []( std::string &str ){ std::cout << str << '\n'; };
+
+  // C-Style comment
   auto comment_c = 
     ("/*"_p).unused() >>
-    (*( eh::parser::any - "*/"_p )).string()[printstring] >>
+    (*( eh::parser::any - "*/"_p )) >>
     ("*/"_p).unused()
     ;
 
+  // Cpp-Style comment
   auto comment_cpp =
     "//"_p >>
     *( eh::parser::any - '\n'_p ) >>
@@ -32,70 +128,18 @@ int main()
     "and /*this is hiding too*/ this is\n"
     "multi line\n";
   auto b = str.begin();
+
+  // return optional< vector<char> >
   auto ret = comment_out.parse( b , str.end() );
   for( auto i : ret.get() )
   {
     std::cout << i;
   }
-#endif
+}
 
-#if 0
-  // tokeninze
-  std::string str = "   11 1aa    a2 22b     bb    ";
-  auto white = ' '_p;
-  auto parser = 
-    eh::parser::alpha[( [](int x){return 0;})] |
-    eh::parser::digit[( [](int x){return 1;})];
-  auto tokens = eh::parser::tokenize( parser , white , str.begin() , str.end() );
-  for( auto i : tokens )
-  {
-    if( i.data == 0 )
-    {
-      std::cout << "alpha " << *i.begin << std::endl;
-    }else if( i.data == 1 )
-    {
-      std::cout << "digit " << *i.begin << std::endl;
-    }else
-    {
-      std::cout << "unknown\n";
-    }
-  }
-  auto b = str.begin();
-  auto aa = (*('_'_p.unused())).parse( b , str.end() );
-#endif
-
-#if 0
-  eh::parser::dictionary<int,char> dict;
-  dict.add( std::string("hello") , 0 );
-  dict.add( std::string("hell") , 2 );
-  dict.add( std::string("world") , 1 );
-  dict.add( std::string("hellowo") , 4 );
-
-  std::string str = "helloworld";
-  auto b = str.begin();
-  auto ret = dict.parse( b , str.end() );
-  if( ret )
-  {
-    std::cout << ret.get();
-  }else
-  {
-    std::cout << "fail";
-  }
-
-#endif
-
-  std::string str = "abcabc";
-  auto b = str.begin();
-
-  auto ap = 'a'_p.unused()[(
-      []()
-      {
-      std::cout << "a!\n";
-      } )];
-
-  auto p = ( ap >> 'b'_p >> 'x'_p ) |
-    (ap >> 'x'_p) |
-    ( eh::parser::repeat( ap , 3 , -1 ) ) |
-    (ap >> 'b'_p >> 'c'_p);
-  auto ret = p.parse( b , str.end() );
+int main()
+{
+  //variable_name_parse();
+  //tokenize();
+  comment_parser();
 }
