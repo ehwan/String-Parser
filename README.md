45d19a70c0df72f24bfd04284d338afaaf8adf86
Taehwan Kim <ehwank98@gmail.com>
2023-08-21 11:40:50 +0900

    change calculator.cpp to readable example and add it to README.md Overview

--- README.md
+++ README.md
@@ -1,6 +1,103 @@
 # String Parser
 Header only template-based string parser and pattern matcher.
 
+fully supported with clang / c++14
+
+## Overview
+```cpp
+// @FileName : examples/calculator.cpp
+
+namespace ep = eh::parser;
+
+int main()
+{
+  // REGEX : [0-9]
+  // Pattern Match and return its digit as integer type
+  auto digit_parser = 
+    ep::action(
+      ep::range( '0', '9' ),
+
+      // captured Attribute of range() is decltype(*iterator) = char
+      // this functor will be called every range() pattern matched
+      []( char captured_character )
+      {
+        // return char->digit
+        // this will be new Attribute of this parser
+        return (int)(captured_character - '0');
+      }
+    );
+
+  // REGEX : [0-9]+
+  auto integer_parser =
+    ep::action(
+      ep::repeat( digit_parser, 1, 12 ),
+
+      // captured Attribute of repeat() is vector< child Attribute > = vector<int>
+      []( std::vector<int> &captured_digits )
+      {
+        int result = 0;
+        for( int d : captured_digits )
+        {
+          result = result * 10 + d;
+        }
+        // return accumulated integer;
+        // this will be new Attribute of this parser
+        return result;
+      }
+    );
+
+  // REGEX : integer_parser ( ('+'|'-') integer_parser )*
+  auto plus_minus_expression_noaction =
+    ep::seq( // Attribute = tuple< Attr of child > = tuple< int, vector< tuple<char,int> > >
+      integer_parser, // Attribute = int
+      ep::repeat( // Attribute = vector< Attr of child > = vector< tuple<char,int> >
+        ep::seq( // Attr = tuple< Attrs of child > = tuple< char, int >
+          ep::or_(  // char ( only if all child's Attr are same )
+            ep::one('-'), // char
+            ep::one('+') // char
+          ),
+          integer_parser // int
+        ),
+        0, 10000000
+      )
+    );
+  auto plus_minus_expression =
+    ep::action(
+      plus_minus_expression_noaction,
+      // captured tuple will be automatically unpacked
+      []( int first_captured_integer, std::vector< std::tuple<char,int> > const& rhs_captured )
+      {
+        int result = first_captured_integer;
+
+        // process right-hand-side operations
+        for( auto tup : rhs_captured )
+        {
+          char operation = std::get<0>( tup );
+          int rhs_integer = std::get<1>( tup );
+
+          if( operation == '+' ){ result += rhs_integer; }
+          else if( operation == '-' ){ result -= rhs_integer; }
+        }
+
+        // return calculated integer;
+        // this will be new Attribute of this parser
+        return result;
+      }
+    );
+
+    std::string input_string = "1+22-333+4444-55555+666666 unknown string";
+
+    auto begin = input_string.begin();
+    auto parse_result = plus_minus_expression.parse( begin, input_string.end() );
+
+    std::cout << "Input String : " << input_string << "\n";
+    std::cout << "Parse Result : " << std::boolalpha << parse_result.is_valid() << "\n";
+    std::cout << "Parsed Data : " << parse_result.get() << " = " << 1+22-333+4444-55555+666666 << "\n";
+
+    std::cout << "Iterator 'begin' point at : " << std::string( begin, input_string.end() ) << "\n";
+}
+```
+
 ## Examples
 see 'examples/basic.cpp' for basic tutorial
 
--- examples/calculator.cpp
+++ examples/calculator.cpp
@@ -1,89 +1,94 @@
-#include <ios>
 #include <iostream>
 #include <string>
 
 #include "../parser.hpp"
 
-struct accum_t
-{
-  int data;
-  accum_t()
-  {
-    data = 0;
-  }
-  void operator()( int x )
-  {
-    data = data*10 + x;
-  }
-  int operator()() const
-  {
-    return data;
-  }
-};
-
-// This virtual class can be assigned as any parser
-// for recurssive pattern matching
-using rule_t = eh::parser::rule<int,std::string::iterator>;
+namespace ep = eh::parser;
 
 int main()
 {
-  auto num =
-    eh::parser::action(
-    eh::parser::repeat( eh::parser::digit0, 1, 10000 ),
-    []( auto &v )
-    {
-      int x = 0;
-      for( int i : v )
+  // REGEX : [0-9]
+  // Pattern Match and return its digit as integer type
+  auto digit_parser = 
+    ep::action(
+      ep::range( '0', '9' ),
+
+      // captured Attribute of range() is decltype(*iterator) = char
+      // this functor will be called every range() pattern matched
+      []( char captured_character )
       {
-      x = x *10 + i-'0';
-      }
-      return x;
-    } );
-  rule_t expr;
-  rule_t expr0 = num;
-  rule_t expr1;
-  expr1 =
-    eh::parser::action(
-      expr0.ref() 
-      >> eh::parser::repeat(
-        (('*'_p).unused() >> expr0.ref()),
-        0,
-        100000
-      ),
-      []( int e1, std::vector<int> &rights )
-      {
-        for( int i : rights )
-        {
-          e1 *= i;
-        }
-        return e1;
-      }
-    );
-  rule_t expr2;
-  expr2 =
-    eh::parser::action(
-      expr1.ref() 
-      >> eh::parser::repeat(
-        (('+'_p).unused() >> expr1.ref()),
-        0,
-        100000
-      ),
-      []( int e1, std::vector<int> &rights )
-      {
-        for( int i : rights )
-        {
-          e1 += i;
-        }
-        return e1;
+        // return char->digit
+        // this will be new Attribute of this parser
+        return (int)(captured_character - '0');
       }
     );
 
-  expr = expr2;
+  // REGEX : [0-9]+
+  auto integer_parser =
+    ep::action(
+      ep::repeat( digit_parser, 1, 12 ),
 
-  std::string str = "1+2*3";
-  auto begin = str.begin();
-  auto result = expr.parse( begin, str.end() );
-  std::cout << "Input String : " << str << "\n";
-  std::cout << "Parse Result : " << std::boolalpha << result.is_valid() << "\n";
-  std::cout << "Parse Data : " << result.get() << "\n";
+      // captured Attribute of repeat() is vector< child Attribute > = vector<int>
+      []( std::vector<int> &captured_digits )
+      {
+        int result = 0;
+        for( int d : captured_digits )
+        {
+          result = result * 10 + d;
+        }
+        // return accumulated integer;
+        // this will be new Attribute of this parser
+        return result;
+      }
+    );
+
+  // REGEX : integer_parser ( ('+'|'-') integer_parser )*
+  auto plus_minus_expression_noaction =
+    ep::seq( // Attribute = tuple< Attr of child > = tuple< int, vector< tuple<char,int> > >
+      integer_parser, // Attribute = int
+      ep::repeat( // Attribute = vector< Attr of child > = vector< tuple<char,int> >
+        ep::seq( // Attr = tuple< Attrs of child > = tuple< char, int >
+          ep::or_(  // char ( only if all child's Attr are same )
+            ep::one('-'), // char
+            ep::one('+') // char
+          ),
+          integer_parser // int
+        ),
+        0, 10000000
+      )
+    );
+  auto plus_minus_expression =
+    ep::action(
+      plus_minus_expression_noaction,
+      // captured tuple will be automatically unpacked
+      []( int first_captured_integer, std::vector< std::tuple<char,int> > const& rhs_captured )
+      {
+        int result = first_captured_integer;
+
+        // process right-hand-side operations
+        for( auto tup : rhs_captured )
+        {
+          char operation = std::get<0>( tup );
+          int rhs_integer = std::get<1>( tup );
+
+          if( operation == '+' ){ result += rhs_integer; }
+          else if( operation == '-' ){ result -= rhs_integer; }
+        }
+
+        // return calculated integer;
+        // this will be new Attribute of this parser
+        return result;
+      }
+    );
+
+    std::string input_string = "1+22-333+4444-55555+666666 unknown string";
+
+    auto begin = input_string.begin();
+    auto parse_result = plus_minus_expression.parse( begin, input_string.end() );
+
+    std::cout << "Input String : " << input_string << "\n";
+    std::cout << "Parse Result : " << std::boolalpha << parse_result.is_valid() << "\n";
+    std::cout << "Parsed Data : " << parse_result.get() << " = " << 1+22-333+4444-55555+666666 << "\n";
+
+    std::cout << "Iterator 'begin' point at : " << std::string( begin, input_string.end() ) << "\n";
 }
